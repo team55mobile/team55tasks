38bf6d86f9c437666f5ac9de263f94606dcca8ca
Butakov Igor <butakov.Igor@team55.ru>
2021-12-01 23:15:01 +0600

    check build

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class TasksApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo !!',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -25,7 +25,7 @@ class TasksApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(title: 'Flutter Demo Home Page !!'),
     );
   }
 }
